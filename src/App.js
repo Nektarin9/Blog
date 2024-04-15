2f216cf36e30bd413e063c81e67c4523b56eff6e
Nektarin <vlad-kasparov@mail.ru>
2024-04-15 13:00:52 +0300

    подготовка проекта

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {styled} from 'styled-components';
+
+const Div = styled.header`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<div>12gyллллulgyul3</div>
+			<i className="fa fa-camera-retro"></i>
+			<i className="fa fa-spinner fa-4x" aria-hidden="true"></i>
+
+
+		</Div>
+	);
+};
